19b1943bafca64acc187e7163f303ee7ea9722e8
Геогрий Макаров <gera54332@gmail.com>
2022-06-21 12:56:31 +0300

    Changes

--- Assets/Scripts/Gera/MainCharControl.cs
+++ Assets/Scripts/Gera/MainCharControl.cs
@@ -3,7 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainCharControl : MonoBehaviour
-{
+{   
+    public enum Move
+    {
+        trPosition,
+        trTranslate,
+        rbForce,
+        rbVelocity,
+        rbMovePosition
+    }
+    
+    public Move move;
+
+    public float speed;
+    float horizontal, vertical;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,11 @@ public class MainCharControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal;
+        position.y = position.y + speed * vertical;
+        transform.position = position;
     }
 }
