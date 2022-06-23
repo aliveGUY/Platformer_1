1ef75854da7e21d90c695bd73df1ab7ea11d742f
aliveGUY <ilyadubrivvin@gmail.com>
2022-06-23 13:17:01 +0300

    fix merge conflict

--- Assets/Scripts/Masha/Envairementmasha.cs
+++ Assets/Scripts/Masha/Envairementmasha.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Envairementmasha : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame 
     void Start()
     {
         
