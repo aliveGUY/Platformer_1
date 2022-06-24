ab8bd2cd0978c8e887d0aa69cf9d2521bd86ac05
aliveGUY <ilyadubrivvin@gmail.com>
2022-06-24 14:21:02 +0300

    Add shooting mechanics

--- Assets/Scripts/Illia/Shooting.cs
+++ Assets/Scripts/Illia/Shooting.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform shootingPoint;
+    public GameObject bulletPrefab;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+    void Shoot() {
+        Debug.Log("Shooting...");
+        Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
     }
 }
